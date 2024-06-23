2a97747abe2f0e6401b4350b3eecbab51167db27
sarthakdev143-lite <sarthakdev143.official@gmail.com>
2024-06-23 23:39:11 +0530

    "Updated package-lock.json, package.json, and User.js files with new dependencies, scripts, and code changes."

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -7,7 +7,7 @@ const User = () => {
     const [position, setPosition] = useState({ top: 50, left: 50 });
     const [keysPressed, setKeysPressed] = useState({});
     const [speed, setSpeed] = useState(35); // Initial speed (adjust as needed)
-    const [lastBulletTime, setLastBulletTime] = useState(0); // State to track the last bullet fired time
+    const [lastBulletTime, setLastBulletTime] = useState(500); // State to track the last bullet fired time
 
     const animationRef = useRef();
     const lastTimestampRef = useRef(performance.now());
@@ -18,19 +18,20 @@ const User = () => {
         const currentTime = performance.now();
 
         // Check if 0.2 seconds have passed since the last bullet was fired
-        if (currentTime - lastBulletTime < 200) {
+        if (currentTime - lastBulletTime < 500) {
+            console.log('Not enough time passed since last bullet fired');
             return;
-        }
+        } else {
+            setLastBulletTime(currentTime);
 
-        setLastBulletTime(currentTime);
+            const boardRect = clickEvent.currentTarget.getBoundingClientRect();
+            const targetX = (clickEvent.clientX - boardRect.left) / boardRect.width * 100;
+            const targetY = (clickEvent.clientY - boardRect.top) / boardRect.height * 100;
 
-        const boardRect = clickEvent.currentTarget.getBoundingClientRect();
-        const targetX = (clickEvent.clientX - boardRect.left) / boardRect.width * 100;
-        const targetY = (clickEvent.clientY - boardRect.top) / boardRect.height * 100;
-
-        // Call shootBullet function on Bullet component
-        if (bulletRef.current) {
-            bulletRef.current.shootBullet(position.left, position.top, targetX, targetY);
+            // Call shootBullet function on Bullet component
+            if (bulletRef.current) {
+                bulletRef.current.shootBullet(position.left, position.top, targetX, targetY);
+            }
         }
     };
 
@@ -61,8 +62,8 @@ const User = () => {
 
             let { top, left } = position;
             const userSize = 3; // User size in rem
-            const boardHeight = 113; // Board height in percentage
-            const boardWidth = 116; // Board width in percentage
+            const boardHeight = 119; // Board height in percentage
+            const boardWidth = 119; // Board width in percentage
 
             // Calculate movement based on keys pressed
             if (keysPressed['w'] || keysPressed['ArrowUp']) {
